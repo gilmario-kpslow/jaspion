a13a8d2f9f02d5c7f033eb898ba50b09735e150c
antonioallan <antonioallan.santos@gmail.com>
2014-07-06 17:46:59 -0300

    Classe RegistrySession
    Classe que gerencia a variável global $_SESSION

---
+++ jaspion/Util/RegistrySession.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace jaspion\Util;
+
+/**
+ * Description of RegistrySession
+ *
+ * @author allan
+ */
+class RegistrySession {
+    
+    public function __set($name, $value) {
+        $_SESSION[$name] = serialize($value);
+    }
+    
+    public function __get($name) {
+        return unserialize($_SESSION[$name]);
+    }
+    
+    public function unregisty(){
+        session_destroy();
+    }
+}
