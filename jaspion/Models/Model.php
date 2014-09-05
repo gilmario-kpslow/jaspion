5a7077eaeaf6dcd59c28374cc2ebf091c0c15b5d
gilmario-kpslow <gilmariosoftware@gmail.com>
2014-09-05 15:28:28 -0300

    ajuste no controller

--- jaspion/Models/Model.php
+++ jaspion/Models/Model.php
@@ -1,36 +1,41 @@
 <?php
+
 namespace jaspion\Models;
+
 /*
  * Anotações possíveis para atributos
  * @columnDb para informar o nome do Coluna no danco de dados
  * @formName para informar o nome do atributo name do input no formulario
  */
+
 use jaspion\Util\AnotacaoUtil;
+
 /**
  * Description of Atributo
  *
  * @author allan
  */
 abstract class Model {
-    
     /*
      * Metodo que retorna o valor da anotação @formName do atributo informado
      */
+
     public function getFormName($atributo) {
         $arrayAnotacoes = AnotacaoUtil::gerarArraydeAnotacaoAtributo($this);
         return $arrayAnotacoes[$atributo]["formName"];
     }
-    
+
     /*
      * Metodo que retorna o valor da anotação @columnDb do atributo informado
      */
+
     public function getBancoName($atributo) {
         $arrayAnotacoes = AnotacaoUtil::gerarArraydeAnotacaoAtributo($this);
         return $arrayAnotacoes[$atributo]["columnDb"];
     }
 
     /*
-     * Metodo que gera um array de associativo, tendo como chave o nome da 
+     * Metodo que gera um array de associativo, tendo como chave o nome da
      * anotação @columnDb do atributo e como valor o valor do atributo,
      * para que seja enviado ao DAO do modelo
      */
@@ -52,26 +57,23 @@ abstract class Model {
     }
 
     /*
-     * Metodo recebe uma resultado de uma query do banco e 
+     * Metodo recebe uma resultado de uma query do banco e
      * seta esse valores no seu respectivo atributo
      */
 
     public function popularBanco(Array $dados) {
         $arrayAnotacoes = AnotacaoUtil::gerarArraydeAnotacaoAtributo($this);
         foreach ($arrayAnotacoes as $campo => $anotacoes) {
-            foreach ($anotacoes as $anotacao => $valor) {
-                if ($anotacao == 'columnDb') {
-                    if (key_exists($valor, $dados)) {
-                        $set = 'set' . ucfirst($campo);
-                        $this->$set($dados[$valor]);
-                    }
-                }
+            $valor = $anotacoes['columnDb'];
+            if (!is_null($valor) && key_exists($valor, $dados)) {
+                $set = 'set' . ucfirst($campo);
+                $this->$set($dados[$valor]);
             }
         }
     }
 
     /*
-     * Metodo recebe os valores de um formulario e 
+     * Metodo recebe os valores de um formulario e
      * seta esse valores no seu respectivo atributo
      */
 
@@ -79,18 +81,28 @@ abstract class Model {
         $arrayAnotacoes = AnotacaoUtil::gerarArraydeAnotacaoAtributo($this);
         if (!is_null($arrayAnotacoes)) {
             foreach ($arrayAnotacoes as $campo => $anotacoes) {
-                foreach ($anotacoes as $anotacao => $valor) {
-                    if ($anotacao == 'formName') {
-                        if (key_exists($valor, $dados)) {
-                            $set = 'set' . ucfirst($campo);
-                            $this->$set($dados[$valor]);
-                        }
+                $valor = (key_exists('formName', $anotacoes) ? $anotacoes['formName'] : "");
+                $tipo = (key_exists('tipo', $anotacoes) ? $anotacoes['tipo'] : "");
+                if (!is_null($valor) && key_exists($valor, $dados)) {
+                    $set = 'set' . ucfirst($campo);
+                    switch ($tipo) {
+                        case 'int' :
+                            $this->$set(\jaspion\Util\Converter::stringParaInteger($dados[$valor]));
+                            break;
+                        case 'float':
+                            $this->$set(\jaspion\Util\Converter::stringParaDouble($dados[$valor]));
+                            break;
+                        case 'date':
+                            $this->$set(\jaspion\Util\Converter::stringParaData($dados[$valor]));
+                            break;
+                        case 'datetime':
+                            $this->$set(\jaspion\Util\Converter::stringParaDataTime($dados[$valor]));
+                            break;
+                        default :$this->$set($dados[$valor]);
+                            break;
                     }
                 }
             }
-            return array_filter($dados);
-        } else {
-            return null;
         }
     }
 
---
+++ jaspion/Util/Converter.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Converter
+ *
+ * @author gilmario
+ */
+
+namespace jaspion\Util;
+
+class Converter {
+
+    public static function stringParaDouble($valor) {
+        $valor = str_replace(".", "", $valor);
+        $valor = str_replace(",", ".", $valor);
+        return floatval($valor);
+    }
+
+    public static function stringParaInteger($valor) {
+        $valor = str_replace(".", "", $valor);
+        $valor = str_replace(",", ".", $valor);
+        return intval($valor);
+    }
+
+    public static function stringParaData($data) {
+        //dd/mm/yyyy
+        $dataarray = explode("/", $data);
+        return $dataarray[2] . "-" . $dataarray[1] . "-" . $dataarray[0];
+    }
+
+    public static function stringParaDataTime($data) {
+        //dd/mm/yyyy hh:mm:ss
+        $dataarray = explode(" ", $data);
+        return $this->stringParaData($dataarray[0]) . " " . $dataarray[1];
+    }
+
+}
