bfe5c854220228ebfa79a3b690ba19c95c204b08
Gilmário <gilmariosoftware@gmail.com>
2015-03-23 10:52:02 -0300

    Update Conexao.php

--- jaspion/DAO/Conexao.php
+++ jaspion/DAO/Conexao.php
@@ -20,18 +20,17 @@ class Conexao {
     public static function getDb($identificador) {
         $fileconfig = file_get_contents("../App/Config/conexoes.json");
         $parametros = json_decode($fileconfig);
-
-        foreach ($parametros->conexoes as $parametro) {
-            if ($parametro->identificador == $identificador) {
-                $drive =  $parametro->driver;
-                $host = $parametro->host;
-                $dbname = $parametro->dbname;
-                $username = $parametro->username;
-                $password = $parametro->password;
-                break;
+        if(!isset(self::$pdo[$identificador]) || is_null(self::$pdo[$identificador])){
+            foreach ($parametros->conexoes as $parametro) {
+                if ($parametro->identificador == $identificador) {
+                    $drive =  $parametro->driver;
+                    $host = $parametro->host;
+                    $dbname = $parametro->dbname;
+                    $username = $parametro->username;
+                    $password = $parametro->password;
+                    break;
+                }
             }
-        }
-        if(is_null(self::$pdo[$identificador])){
             self::$pdo[$identificador] = new \PDO($drive . ":host=" . $host . ";dbname=" . $dbname, $username, $password);
         }
         return self::$pdo[$identificador];
